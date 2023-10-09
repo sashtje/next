eeb69a928234b6956da615654dcaa5c975719c5d
sashtje <sashtje.lefevre@gmail.com>
2023-10-09 23:00:11 +0300

    feat: add useRouter

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,11 @@
+import {useRouter} from "next/router";
+
 export default function () {
+  const { query } = useRouter();
+
   return (
-    <div>User</div>
+    <div>
+      <h1>User with id = {query.id}</h1>
+    </div>
   );
 }
