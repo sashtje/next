3cdca0a8ee40c739c2d24dc1c88779c3787a3e21
sashtje <sashtje.lefevre@gmail.com>
2023-10-09 23:33:56 +0300

    feat: add and check sass and fix links color

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,11 @@
 import {useRouter} from "next/router";
+import styles from '../../styles/User.module.scss';
 
-export default function () {
+export default function User () {
   const { query } = useRouter();
 
   return (
-    <div>
+    <div className={styles.user}>
       <h1>User with id = {query.id}</h1>
     </div>
   );
