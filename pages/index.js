785b24048376c0cb294f8817f92cf001daa3e714
sashtje <sashtje.lefevre@gmail.com>
2023-10-10 00:01:07 +0300

    feat: add some SEO

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
+import Head from "next/head";
+
 import { NavLink } from "../components/NavLink";
 
 const Index = () => {
   return (
-    <main>
-      <nav className="navbar">
-        <NavLink href="/" text="Main" />
-        <NavLink href="/users" text="Users" />
-      </nav>
+    <>
+      <Head>
+        <meta keywords="nextjs, app"/>
+        <title>Main page</title>
+      </Head>
+      <main>
+        <nav className="navbar">
+          <NavLink href="/" text="Main" />
+          <NavLink href="/users" text="Users" />
+        </nav>
 
-      <h1>Main page</h1>
+        <h1>Main page</h1>
 
-      <style jsx>
-        {`
-          .navbar {
-            background: orange;
-            padding: 15px;
-          }
-        `}
-      </style>
-    </main>
+        <style jsx>
+          {`
+            .navbar {
+              background: orange;
+              padding: 15px;
+            }
+          `}
+        </style>
+      </main>
+    </>
   );
 };
 
