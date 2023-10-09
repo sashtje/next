1eedc65d22cf7977ab6d7b2885c8dd3a6a4e7bc3
sashtje <sashtje.lefevre@gmail.com>
2023-10-09 22:56:18 +0300

    feat: add dynamic routes

--- pages/users.js
+++ pages/users.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import Link from "next/link";
+
 const Users = () => {
+  const [users, setUsers] = useState([
+    {id: 1, name: 'alla'},
+    {id: 2, name: 'sveta'}
+  ])
+
   return (
     <main>
       <h1>Users List</h1>
+      <ul>
+        {
+          users.map(user => (
+            <li key={user.id}><Link href={`/users/${user.id}`}>{user.name}</Link></li>
+          ))
+        }
+      </ul>
     </main>
   );
 };
